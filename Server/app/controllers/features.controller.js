c3b3bc9d99458e94b438d6800a619fbbe2c4ba10
VISHNU984 <54102033+VISHNU984@users.noreply.github.com>
2023-11-03 17:44:01 +0530

    Update features.controller.js
    Vonage API for Message Service

--- Server/app/controllers/features.controller.js
+++ Server/app/controllers/features.controller.js
@@ -1,38 +1,31 @@
-const axios = require('axios');
+const { Vonage } = require('@vonage/server-sdk')
+
 
 exports.sendOTP = (req, res) => {
 
-  // Replace with your Infobip API key and the specific SMS details
-  const infobipApiKey = '7f707663ec85d0fff6ae002dba4fc96b-15c47355-40c2-4f07-8e50-2a53e6df58da';
-  const sender = 'Vishnu';
-  const recipient = '+919842266134';
-  const message = 'Hi from Solver Hands';
-  
-  // API endpoint for sending SMS
-  const apiUrl = 'https://y3pny9.api.infobip.com';
-  
-  // SMS data
-  const smsData = {
-      from: sender,
-      to: recipient,
-      text: message,
-  };
-  
-  // Send SMS using Infobip API
-  axios.post(apiUrl, smsData, {
-      headers: {
-          'Authorization': `App ${infobipApiKey}`,
-          'Content-Type': 'application/json',
-      },
-  })
-  .then(response => {
-      console.log('SMS sent successfully');
-      console.log(response.data);
-  })
-  .catch(error => {
-      console.error('Failed to send SMS');
-      console.error(error);
-  });
-  
+    const vonage = new Vonage({
+        apiKey: "11111",
+        apiSecret: "1111111111111"
+    });
+    const from = "Vonage APIs"
+    const to = "919842266134"
+    const text = 'A text message sent using the Vonage SMS API'
+
+    async function sendSMS() {
+        await vonage.sms.send({ to, from, text })
+            .then(resp => { 
+                console.log('Message sent successfully');
+                 console.log(resp); 
+                res.send("OTP sent Successfully")
+            })
+            .catch(err => {
+                 console.log('There was an error sending the messages.'); 
+                 console.error(err);
+                 res.send("OTP sent Failed")
+            });
+    }
+
+    sendSMS();
+
 };
 
